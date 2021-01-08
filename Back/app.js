7044bdf19fbc1baffc59f5f875235f3768f3d642
tom.willemin <willemintom03@gmail.com>
2021-01-08 13:58:01 +0100

    Init du Back

---
+++ Back/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const articleRoutes = require('./routes/image');
+const path = require('path');
+
+const app = express();
+
+// Va rendre les informations exploitables
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // A changer avec les variables d'environnement
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Permet de donner l'accès aux images
+// app.use('/pictures', express.static(path.join(__dirname, 'pictures')));
+
+// Routes de l'application
+app.use('/api/images', articleRoutes);
+
+module.exports = app;
---
+++ Back/routes/image.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/*const imageController = require('../controller/image');
+const multer = require('../middleware/multer-config');*/
+
+/*router.post('/', multer, imageController.create);
+router.get('/', imageController.getAll);
+router.get('/:id', imageController.getOne);*/
+
+module.exports = router
